66f43fb412022a0d356e2539db11af60ea84eaf9
Muhammad Saad <muhammadsamehsaad@gmail.com>
2024-08-19 02:39:13 +0300

    Update Database

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -5,7 +5,7 @@ namespace Talabat.APIs
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +22,30 @@ namespace Talabat.APIs
             });
 
             var app = builder.Build();
+
+            using var scope = app.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            var _dbContext = services.GetRequiredService<StoreContext>();
+
+
+            //It's services added when we : builder.Services.AddControllers();
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                await _dbContext.Database.MigrateAsync();//Update Database
+
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "An Error Occurred During Apply The Migration ");
+
+                //Console.WriteLine(ex);
+            }
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
