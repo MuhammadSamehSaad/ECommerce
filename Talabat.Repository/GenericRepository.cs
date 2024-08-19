3e3c2fbba2175eaaa8f9959f58903a690dfcef9f
Muhammad Saad <muhammadsamehsaad@gmail.com>
2024-08-19 22:15:13 +0300

    implementGenericRepository

---
+++ Talabat.Repository/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Repository.Data;
+
+namespace Talabat.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext _dbContest;
+
+        public GenericRepository(StoreContext dbContest)
+        {
+            _dbContest = dbContest;
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbContest.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetAsync(int id)
+        {
+            return await _dbContest.Set<T>().FindAsync(id);
+        }
+    }
+}
